ae5236daaa1334885d77ef5d652e73361d357798
potapen <po@ta.pen>
2022-03-24 18:41:36 +0100

    update

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -2,24 +2,91 @@ class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
   }
-
+/*
+  //this works with .then
   getFullList () {
+    return axios({ //we need to return a promise
+      method: 'GET',
+      url: 'http://localhost:8000/characters'
+    })
+      .then(response => {
+        console.log('calling getFullList')
+        const charactersArray = response.data
+        console.log('charactersArray: ', charactersArray)
+        return charactersArray
+      })
+      .catch(err => {
+        // Here we catch the error
+        console.error(error)
+      });
+  }
+*/
 
+  async getFullList () {
+    const response = await axios({ //we need to return a promise
+      method: 'GET',
+      url: 'http://localhost:8000/characters'
+    })
+    return response.data
+  }
+  async getOneRegister (id) {
+    const response = await axios({ //we need to return a promise
+      method: 'GET',
+      baseURL: 'http://localhost:8000/characters',
+      url: id
+    })
+    console.log('response.data: ', response.data)
+    return response.data
   }
 
-  getOneRegister () {
 
-  }
+  async deleteOneRegister (id) {
+    try{
+      const response = await axios({ //we need to return a promise
+        method: 'DELETE',
+        baseURL: 'http://localhost:8000/characters',
+        url: id
+      })
+      console.log('response.status: ', response.status)
+    }
+    catch(error){
+      console.log('----------------------------')
+      console.error(error)
+      }
+    }
+
 
   createOneRegister () {
 
   }
 
-  updateOneRegister () {
+  async updateOneRegister (characterObj) {
+    console.log('inside updateOneRegister')
+    try{
+      let data = JSON.stringify({
+        "name": characterObj.name,
+        "occupation": characterObj.occupation,
+        "weapon": characterObj.weapon,
+        "cartoon": characterObj.cartoon,
+        "id": characterObj.id
+      });
 
-  }
-
-  deleteOneRegister () {
-
-  }
+      let config = {
+        method: 'patch',
+        baseURL: 'http://localhost:8000/characters/',
+        url: characterObj.id,
+        headers: { 
+          'Content-Type': 'application/json'
+        },
+        data : data
+      };
+      
+      const response = await axios(config)
+      console.log('response: ', response)
+    }
+    catch(error){
+      console.log('----------------------------')
+      console.error(error)
+      }
+    }
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,19 +1,83 @@
 const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersContainerElt = document.querySelector('.characters-container');
+const editFormElt = document.querySelector('#edit-character-form');
+
+const createCharacterCard = (characterObject)=>{
+  console.log('characterObject :', characterObject)
+  const characterElt = document.createElement('div')
+  characterElt.classList.add('character-info')
+
+  const nameElt = document.createElement('div')
+  nameElt.innerText = 'Character Name : ' + characterObject.name
+  nameElt.classList.add('name')
+  characterElt.appendChild(nameElt)
+
+  const occupationElt = document.createElement('div')
+  occupationElt.innerText = 'Character Occupation : ' +  characterObject.occupation
+  occupationElt.classList.add('occupation')
+  characterElt.appendChild(occupationElt)
+  
+  const cartoonElt = document.createElement('div')
+  cartoonElt.innerText = 'Is a cartoon? : ' +  characterObject.cartoon
+  cartoonElt.classList.add('cartoon')
+  characterElt.appendChild(cartoonElt)
+  
+  const weaponElt = document.createElement('div')
+  weaponElt.innerText = 'Character Weapon  : ' +  characterObject.weapon
+  weaponElt.classList.add('weapon')
+  characterElt.appendChild(weaponElt)
+
+  return characterElt
+}
+
 
 window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+  document.getElementById('fetch-all').addEventListener('click', async function (event) {
+    console.log('inside fetch-all')
+    const charactersArray = await charactersAPI.getFullList()
+    console.log('charactersArray :', charactersArray)
+    charactersContainerElt.innerHTML=''
+    charactersArray.forEach(character =>{
+      const characterCardElt = createCharacterCard(character)
+      charactersContainerElt.appendChild(characterCardElt)
+    })
+
+
+  });
+  
+  document.getElementById('fetch-one').addEventListener('click', async function (event) {
+    console.log('inside fetch-one')
+    const id = document.querySelector('input[name="character-id"]').value
+    console.log('id is :', id)
+    const characterSingle = await charactersAPI.getOneRegister(id)
+    console.log('characterSingle :', characterSingle)
+    charactersContainerElt.innerHTML=''
+    const characterCardElt = createCharacterCard(characterSingle)
+    charactersContainerElt.appendChild(characterCardElt)
+  });
+
+  document.getElementById('delete-one').addEventListener('click', async function (event) {
+    console.log('inside delete-one')
+    const id = document.querySelector('input[name="character-id-delete"]').value
+    console.log('id is :', id)
+    try{
+      const characterDeleted = await charactersAPI.deleteOneRegister(id)
+    }
+    catch(error){console.error(error)}
+  
 
   });
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('delete-one').addEventListener('click', function (event) {
-
-  });
-
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  document.getElementById('send-data-update').addEventListener('click', async function (event) {
+    // event.preventDefault()
+    const characterObj = {}
+    characterObj.id = editFormElt.querySelector('input[name="chr-id"]').value
+    characterObj.name = editFormElt.querySelector('input[name="name"]').value
+    characterObj.occupation = editFormElt.querySelector('input[name="occupation"]').value
+    characterObj.weapon = editFormElt.querySelector('input[name="weapon"]').value
+    characterObj.cartoon = editFormElt.querySelector('input[name="cartoon"]').value
+    console.log(characterObj)
+    const characterUpdated = await charactersAPI.updateOneRegister(characterObj)
 
   });
 
